5e464691e001046dcece7968e81f1e662ab68449
wfmendez <wuillian.f.mendez@gmail.com>
2024-07-04 19:05:03 -0400

    Actualizando nuestra página de Directorio con un Componente de Directorio y un Componente de Categoria.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
 import CategoryItem from '../category-item/category-item.component';
 import './directory.styles.scss'
-import categoriesData from './categories-data.json'
+import directoryData from './directory-data.json'
 
 const Directory = () => {
     return (
-        <div className="categories-container">
-            {categoriesData.map((category) => (
+        <div className="directory-container">
+            {directoryData.map((category) => (
                 <CategoryItem key={category.id} category={category}/>
             ))}
         </div>
